d5fd88c670a7181db79c4405a147ee78d9832758
Matt Clark <matt.clark@theamx.com>
2024-04-24 12:33:46 +0100

    Renamed init

--- src/register_types.h
+++ src/register_types.h
@@ -5,7 +5,7 @@
 
 using namespace godot;
 
-void initialize_example_module(ModuleInitializationLevel p_level);
-void uninitialize_example_module(ModuleInitializationLevel p_level);
+void initialize_opencv_module(ModuleInitializationLevel p_level);
+void uninitialize_opencv_module(ModuleInitializationLevel p_level);
 
 #endif // GDEXAMPLE_REGISTER_TYPES_H
