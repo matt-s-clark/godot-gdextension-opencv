4b027a2471bfc9d54f7458e9642a21077715f249
Matt Clark <matt.clark@theamx.com>
2024-05-03 14:33:18 +0100

    Working imread

--- demo/opencv-image.gd
+++ demo/opencv-image.gd
@@ -3,12 +3,15 @@ extends TextureRect
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("READY!")
-	var gdExample = CVMat.new();
-	var tiltFiveIRCameraImage = gdExample.get_godot_image();
-	print(tiltFiveIRCameraImage);
-
-	texture = ImageTexture.create_from_image(tiltFiveIRCameraImage);
+	print("READY NOW!")
+	var matLoad = CVImgCodecs.imread("aruco-capture.png", 1);
+	print(matLoad);
+	
+	#var gdExample = CVMat.new();
+	#var tiltFiveIRCameraImage = gdExample.get_godot_image();
+	#print(tiltFiveIRCameraImage);
+#
+	#texture = ImageTexture.create_from_image(tiltFiveIRCameraImage);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
