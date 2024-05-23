f79682048d75f32951b71e524a2e750e4dbe10ee
Matt Clark <matt@vibeapps.com>
2024-05-23 14:49:20 +0100

    Marker Helpers - WiP

--- demo/opencv-image.gd
+++ demo/opencv-image.gd
@@ -4,17 +4,25 @@ extends TextureRect
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("READY NOW!")
-	var matLoad = CVImgCodecs.imread("aruco-capture.png", 1);
-	print(matLoad);
+	var matLoad = CVImgCodecs.imread("aruco-capture.png", 1)
 	
-	var gdExample = CVMat.new();
-	var tiltFiveIRCameraImage:Image = matLoad.get_image();
-	print(tiltFiveIRCameraImage);
+	print(matLoad)
+	print('=========')
+	print(matLoad.get_image())
+	
+	var gdExample = CVMat.new()
+	var tiltFiveIRCameraImage:Image = matLoad.get_image()
+	print(tiltFiveIRCameraImage)
 	
 	if not tiltFiveIRCameraImage.is_empty():
-		texture = ImageTexture.create_from_image(tiltFiveIRCameraImage);
+		texture = ImageTexture.create_from_image(tiltFiveIRCameraImage)
 
-
+	var arucoPos:Array = CVArucoHelper.getMarkerPosition()
+	for aPos in arucoPos:
+		print("ARUCO: ",aPos.x, ", ", aPos.y)
+	
+	$MarkerHighlight.set_position(arucoPos[0])
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
--- src/register_types.h
+++ src/register_types.h
@@ -1,6 +1,7 @@
 #ifndef GDEXAMPLE_REGISTER_TYPES_H
 #define GDEXAMPLE_REGISTER_TYPES_H
 
+#include "CVArucoHelper.h"
 #include "CVCore.h"
 #include "CVImgCodecs.h"
 #include "CVMat.h"
