f2bfe83cb7a0dc69f99ccdc9993a1c802c9929aa
RafaDdS <rafunga@gmail.com>
2024-11-19 16:05:16 -0300

    OpenCV Setup bash
    - Removing submodules

---
+++ opencvSetup.sh
@@ -0,0 +1,26 @@
+#Change version here
+version="4.9.0"
+
+linkToOpenCVDownload="https://github.com/opencv/opencv/archive/refs/tags/${version}.zip"
+linkToContribDownload="https://github.com/opencv/opencv_contrib/archive/refs/tags/${version}.zip"
+
+cd opencv
+
+# Downloading opencv from $linkToOpenCVDownload
+
+wget -O opencv.zip $linkToOpenCVDownload
+unzip -o opencv.zip
+mv -f opencv-${version} opencv
+
+# Downloading opencv contrib modules from $linkToContribDownload
+
+wget -O contrib.zip $linkToContribDownload
+unzip -o contrib.zip
+mv -f opencv_contrib-${version} contrib
+
+mkdir build
+cd build
+
+cmake -DOPENCV_EXTRA_MODULES_PATH=../contrib/modules ../opencv -DCMAKE_INSTALL_PREFIX:PATH=/home/dias/Documentos/Godot/godot-gdextension-opencv/opencv/install
+make
+make install
